ae054aebf97010ff95124e28f04fa5d744510e3b
laxmi <gadhvilaxmi26@gmail.com>
2026-06-18 18:54:01 +0530

    update databse settings

--- config/settings.py
+++ config/settings.py
@@ -84,20 +84,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-if os.environ.get("DATABASE_URL"):
-    DATABASES = {
-        "default": dj_database_url.parse(
-            os.environ.get("DATABASE_URL")
-        )
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-
+DATABASES = {
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
+}
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
