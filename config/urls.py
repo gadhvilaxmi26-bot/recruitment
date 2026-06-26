ae314fa3ed6b4f59c89f9f8c1517fa9a71d34244
laxmi <gadhvilaxmi26@gmail.com>
2026-06-26 10:01:07 +0530

    fix sitemap

--- config/urls.py
+++ config/urls.py
@@ -6,12 +6,15 @@ from django.contrib.sitemaps.views import sitemap
 from django.contrib.sitemaps import GenericSitemap
 from jobportal.sitemaps import StaticViewSitemap
 
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('jobportal.urls')),
-    path('sitemap.xml',sitemap,{'sitemaps':{StaticViewSitemap}}, name='sitemap'),
-]
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+ ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
